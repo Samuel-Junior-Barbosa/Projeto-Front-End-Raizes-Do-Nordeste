50cc5ea7cf9ddd39e343a6d8a5e9fa80b357522f
limin <limin@limin.com>
2026-06-23 23:41:25 -0300

    + Voltando a rota do github

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: '/projeto-fond-end-pagina/',
-  base: '/',
+  base: '/projeto-fond-end-pagina/',
+  //base: '/',
   server : {
     host: true
   },
